c6a1f0de172f2177c0794cd16f00ef44058caa7b
vhstavares <vh.st1503@outlook.com>
2022-10-12 20:50:26 -0300

    Commit Final

--- carford/app/tests.py
+++ carford/app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+import unittest
+from .models import Person, Vehicle
 
 # Create your tests here.
+class PersonTestCase(unittest.TestCase):
+    def setUp(self):
+        self.person = Person.objects.create(
+            name = "Maria",
+            birth = "1987-01-02",
+            document = "325.325.452-52",
+            email = "teste@teste.com",
+            phone = "(15) 8943-2213",
+            sale_opportunity = True
+        )
+        self.vehicle = Vehicle.objects.create(
+            color = 'gray',
+            model_vehicle = 'sedan',
+            name = 'Corrola',
+            plate = 'MKL326',
+            year = '2016',
+            owner = self.person
+        )
+    
+    def testPerson(self):
+        self.assertEquals(self.person.sale_opportunity, True)
+    
+    def testVehicle(self):
+        self.assertEquals(self.vehicle.model_vehicle, 'sedan')
